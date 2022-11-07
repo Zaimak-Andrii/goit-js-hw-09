31c5ec710785c3e39431d110dc8ac0c49a939e34
Andrii Zaimak <dev.andrii.zaimak@gmail.com>
2022-11-07 16:54:27 +0200

    Done 1  task.

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,53 @@
+const refs = {
+  body: document.body,
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+};
+const interval = {
+  id: null,
+  start(cb, delay) {
+    if (this.id) return;
 
+    this.id = setInterval(cb, delay);
+  },
+  stop() {
+    if (!this.id) return;
+
+    clearInterval(this.id);
+    this.id = null;
+  },
+};
+
+setStartButtonEnabled(true);
+
+refs.start.addEventListener('click', startInterval);
+refs.stop.addEventListener('click', stopInterval);
+
+function startInterval() {
+  setStartButtonEnabled(false);
+  changeBodyBacgroundColor();
+
+  interval.start(changeBodyBacgroundColor, 1000);
+}
+
+function stopInterval() {
+  setStartButtonEnabled(true);
+  interval.stop();
+}
+
+function setStartButtonEnabled(enabledStart) {
+  const { start, stop } = refs;
+
+  start.disabled = !enabledStart;
+  stop.disabled = enabledStart;
+}
+
+function changeBodyBacgroundColor() {
+  refs.body.style = `background-color: ${getRandomHexColor()}`;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
